d15cbcf42f9fbeb617da153c2d8a1e58d96ede75
Ian Doyle <iandoyle01@gmail.com>
2018-11-01 21:32:53 +0000

    changed to return a dict

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -5,18 +5,18 @@ import pymysql
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
-#connect to the database
+#connect to the dgiyatabase
 connection = pymysql.connect(host='localhost',
                             user=username,
                             password='',
                             db='Chinook')
 try:
     # Run a query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = "SELECT * FROM Genre;"
         cursor.execute(sql)
-        result = cursor.fetchall() #brings data back
-        print(result)
+        for row in cursor:
+            print(row)
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
