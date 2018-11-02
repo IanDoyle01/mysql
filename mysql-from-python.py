7a147b2b6bdd8dae1f7ee3e7347bf1315eb6251b
Ian Doyle <iandoyle01@gmail.com>
2018-11-02 06:18:50 +0000

    changed to add a SQL table

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -12,11 +13,11 @@ connection = pymysql.connect(host='localhost',
                             db='Chinook')
 try:
     # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                       Friends(name char(20), age int, DOB datetime);""")
+        # Note that the abpve will still display a warning (not error) if the
+        # table already exists
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
