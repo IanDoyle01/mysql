75b9657f27972e7e1db08e6f88202311cc58446c
Ian Doyle <iandoyle01@gmail.com>
2018-11-02 06:30:44 +0000

    changed to insert many rows

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,8 +14,10 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        row = ("Bob", 21, "1990-02-06 23:04:56")
-        cursor.execute("INSERT INTO Friends VALUES(%s, %s, %s);", row)
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 13:12:45"),
+                ("Fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES(%s, %s, %s);", rows)
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
