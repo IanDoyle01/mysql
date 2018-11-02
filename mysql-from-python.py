21af35c5fc29d5afb3df20aecf5d1e56b77b7ac1
Ian Doyle <iandoyle01@gmail.com>
2018-11-02 06:42:25 +0000

    Changed to update many rows

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,8 +13,10 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age= %s WHERE name= %s;",
-                        (23, "Bob"))
+        rows = [(23, 'Bob'),
+                (24, "Jim"),
+                (25, "Fred")]
+        cursor.executemany("UPDATE Friends SET age= %s WHERE name= %s;",rows)
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
