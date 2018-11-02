2559aef193a40164b8a7a15ec25a3fac53576039
Ian Doyle <iandoyle01@gmail.com>
2018-11-02 06:45:41 +0000

    changed to a delete command

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,10 +13,7 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        rows = [(23, 'Bob'),
-                (24, "Jim"),
-                (25, "Fred")]
-        cursor.executemany("UPDATE Friends SET age= %s WHERE name= %s;",rows)
+        rows = cursor.execute('DELETE FROM Friends WHERE name= "Bob";')
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
