2623b4148853844a1f545c35b40862cc71253e3c
Ian Doyle <iandoyle01@gmail.com>
2018-11-02 06:26:21 +0000

    changed to an INSERT row command

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,10 +14,9 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                       Friends(name char(20), age int, DOB datetime);""")
-        # Note that the abpve will still display a warning (not error) if the
-        # table already exists
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES(%s, %s, %s);", row)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
