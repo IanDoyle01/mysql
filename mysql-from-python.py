9daeb8a41b2833d68dc35e6f3526ea0df4815ea2
Ian Doyle <iandoyle01@gmail.com>
2018-11-03 10:45:52 +0000

    changed to delete from a list of any length

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,7 +13,10 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        rows = cursor.execute("DELETE FROM Friends WHERE name=%s;", 'bob')
+        list_of_names = ['Jim', 'Bob']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
